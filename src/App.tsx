2d23414275a62e3e9760289dcb6c151b00313081
Sam <samwentupp@outlook.com>
2023-06-22 21:28:28 -0700

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import './App.css';
+import { TextField } from '@mui/material';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
 
 function App() {
   return (
-    <></>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className='max-w-3xl mx-auto text-center'>
+        <h1 className='mt-2 font-ysabeau text-4xl mb-8 text-white'>Weather App</h1>
+        <form className='w-full mt-2 mb-2'>
+          <TextField id="standard-basic" variant="standard" className='w-full' onChange={(e)=>{console.log(e.target.value)}}/>
+        </form> 
+      </div>
+    </ThemeProvider>
+
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ysabeau: ['Ysabeau SC', 'sans-serif']
+        },
+    },
+  },
+  plugins: [],
+}
